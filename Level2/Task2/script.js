2b1e2f4d5d6a2552e870aa62908b1008dfc389db
Deepika Myakal <99346956+deepikamyakal@users.noreply.github.com>
2024-09-17 14:31:03 +0530

    Add files via upload

---
+++ Level2/Task2/script.js
@@ -0,0 +1,5 @@
+// Toggle the navbar menu on small screens
+document.getElementById('navbarBurger').addEventListener('click', function() {
+    var menu = document.getElementById('navbarMenu');
+    menu.classList.toggle('active');
+});
